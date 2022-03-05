3500be6f653e97adb112e72c2ce5fcb776a06ac3
jagveer <jagveersingh.55555@gmail.com>
2022-03-05 06:28:53 +0530

    commit of start position

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -2,4 +2,10 @@ package snakeandladder;
 
 public class SnakeAndLadder {
 
+	    public static void main(String[] args){
+        System.out.println("Snake and Ladder ");           
+        System.out.println("-------------------------------");
+        int startPosition = 0;
+        System.out.println("Player starting on: " + startPosition);
+}
 }
