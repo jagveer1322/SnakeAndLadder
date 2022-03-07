2e4008f53dbf2fa2a7450c1695dcb9bed74ec21b
jagveer <jagveersingh.55555@gmail.com>
2022-03-07 16:41:48 +0530

    commit

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -18,46 +18,50 @@ public class SnakeAndLadder {
 	public static void main(String[] args){
         System.out.println("Snake and Ladder ");           
         System.out.println("-------------------------------");
-        Scanner input = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int startPosition = 0;
         int currentPosition = 0;
-        System.out.println("Player starting on: " + startPosition);
+        System.out.println("Player is starting at: " +startPosition);
         while (currentPosition != 100) {
-        System.out.println("Enter 1 to roll a die");
-        int die = input.nextInt();
-        if(die == 1){
-            int playerDie = getRandomDieValue();
-            System.out.println("-------------------------------");
-            System.out.println("Die Face: " +playerDie);
-            int option = getRandomOption();
-            switch (option) {
-                case IS_LADDER:
-                    System.out.println("Option: LADDER");
-                    currentPosition += playerDie;
+            System.out.println("Enter 1 to roll a die");
+            int die = in.nextInt();
+            if (die == 1) {
+                int playerDie = getRandomDieValue();
+                System.out.println("Die Face: " + playerDie);
+                int option = getRandomOption();
+                switch (option) {
+                    case IS_LADDER:
+                        System.out.println("Option: LADDER");
+                        currentPosition += playerDie;
+                        break;
+                    case IS_SNAKE:
+                        System.out.println("Option: SNAKE");
+                        currentPosition -= playerDie;
+                        break;
+                    default:
+                        System.out.println("Option: NO PLAY");
+                        break;
+                }
+                if (currentPosition == 100) {
+                    System.out.println("Position: " + currentPosition);
+                    System.out.println("You Won!");
                     break;
-                case IS_SNAKE:
-                    System.out.println("Option: SNAKE");
+                } else if (currentPosition > 100) {
                     currentPosition -= playerDie;
-                    break;
-                default:
-                    System.out.println("Option: NO PLAY");
-                    break;
-            }
-            if (currentPosition == 100) {
-                System.out.println("Position: " + currentPosition);
-                System.out.println("You Won!");
-                break;
-            } else if (currentPosition < 0) {
-                currentPosition = startPosition;
-                System.out.println("Back to Start");
-                System.out.println("Current Position: " + currentPosition);
+                    System.out.println("Invalid move! You are at the same position.");
+                    System.out.println("Current Position: " + currentPosition);
+                } else if (currentPosition < 0) {
+                    currentPosition = startPosition;
+                    System.out.println("Back to Start");
+                    System.out.println("Current Position: " + currentPosition);
+                } else {
+                    System.out.println("Current Position: " + currentPosition);
+                }
+                System.out.println("-------------------------------");
             } else {
-                System.out.println("Current Position: " + currentPosition);
+                System.exit(0);
             }
-            System.out.println("-------------------------------");
-            } else {
-            System.exit(0);
-                   }
-           }
         }
+    }
 }
+       
